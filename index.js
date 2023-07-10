a5eaff0333eb12fca1e7a74f2621edfb48a5db18
kulsoomrasheed <rasheedamaan111@gmail.com>
2023-07-10 12:54:43 +0530

    mock server created

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+const cors = require('cors');
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const port = process.env.Port|| 4000
+server.use(cors());
+server.use(middlewares)
+server.use(router)
+server.listen(port, () => {
+  console.log('JSON Server is running on '+port)
+})
